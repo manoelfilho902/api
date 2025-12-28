360e7a0ca0786485e666f40198b5946c986d7af5
Manoel Batista <manoelbatista902@gmail.com>
2025-12-28 07:23:11 -0300

    initial commit

---
+++ src/main/java/com/github/manoelfilho902/simple/spring/api/configuration/Dataset.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.github.manoelfilho902.simple.spring.api.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author mbatista
+ */
+@Configuration
+public class Dataset {
+    
+
+}
